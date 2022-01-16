5c9f86de67f9f96aeb74436c90968af2292b9bc3
jakestrauss <jake.strauss217@gmail.com>
2022-01-15 16:04:00 -0800

    Changes width for mobile

--- src/static/slideshow_images/fall21Images.jsx
+++ src/static/slideshow_images/fall21Images.jsx
@@ -3,8 +3,7 @@ const fall21Images = [
     'https://storage.googleapis.com/summer_of_jake_map_pictures/fall_21/IMG_1398-min.jpg',
     'https://storage.googleapis.com/summer_of_jake_map_pictures/fall_21/IMG_1413-min.jpg',
     'https://storage.googleapis.com/summer_of_jake_map_pictures/fall_21/IMG_1486-min.jpg',
-    'https://storage.googleapis.com/summer_of_jake_map_pictures/fall_21/IMG_1524-min.jpg',
-    'https://storage.googleapis.com/summer_of_jake_map_pictures/fall_21/IMG_1530-min.jpg'
+    'https://storage.googleapis.com/summer_of_jake_map_pictures/fall_21/IMG_1524-min.jpg'
 ]
 
 export default fall21Images;
